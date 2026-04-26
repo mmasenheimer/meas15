b689a86f7192287611e1cb3fbea4d47803c39356
mmasenheimer <mmasenheimer21@gmail.com>
2026-04-26 11:00:22 -0700

    added last changes

--- src/backend/routes/profile.js
+++ src/backend/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../../../models");
-export default function Map({ user }) {
+
 
 // GET /api/profile?userId=...
 router.get("/", async (req, res) => {
