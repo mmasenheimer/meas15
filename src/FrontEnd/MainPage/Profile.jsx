6022e72f55f660ee8d10a4e7659230d67054b482
AlecSch1 <alecschmitt@arizona.edu>
2026-04-26 03:22:19 -0700

    only one group now

--- src/FrontEnd/MainPage/Profile.jsx
+++ src/FrontEnd/MainPage/Profile.jsx
@@ -3,26 +3,32 @@ import React, { useState } from "react";
 export default function Profile({ logOut, user }) {
     const [joinGroup, setNewGroup] = useState("");
     const [createGroup, setCreateGroup] = useState("");
+    const [inAGroup, setInGroup] = useState(!!user.groups);
 
     const createGroup = () => {
         
+    }
+
+    const leaveGroup = () => {
+
     }
 
     return (
         <div id="profile">
         <div id="userName">Username: {user?.username}</div>
         <div id="points">Points: {user?.points}</div>
-        <div>Groups: </div>
-        <div id="groups">
-            {user.groups?.map((group) => <div>{group}</div>) || "No groups joined"}
-        </div>
+        <div>Group: {!!user.group ? user.group : "No Group joined"}</div>
         <button onClick={() => logOut()}>Log Out</button>
-        <input
+        {!inAGroup && <div id = 'createGroup'>
+            <input
             type="text"
             value={createGroup}
             onChange={(e) => setCreateGroup(e.target.value)}
-        />
-        <button onClick={() => createGroup()}>Create Group</button>
+            />
+            <button onClick={() => createGroup()}>Create Group</button>
+        </div>}
+        {!!inAGroup && <button onClick={() => joinGroup()}>Join Group</button>}
+        {inAGroup && <button onClick={() => leaveGroup()}>Leave Group</button>}
         </div>
     );
 }
