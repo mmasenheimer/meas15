952360ef87542a63306270f5b4522ad7fb908aaf
AlecSch1 <alecschmitt@arizona.edu>
2026-04-25 23:43:44 -0700

    create account in loginpage.jsx

--- src/FrontEnd/LogInPage.jsx
+++ src/FrontEnd/LogInPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function LogInPage({ onSubmit }) {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [error, setError] = useState("");
 
   const handleSubmit = async () => {
@@ -16,11 +17,39 @@ function LogInPage({ onSubmit }) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, password}),
       });
 
       if (!response.ok) {
         throw new Error("Login failed");
+      }
+
+      const data = await response.json();
+      onSubmit(data);
+      setError("");
+    } catch (err) {
+      setError(err.message || "An error occurred during login");
+    }
+  };
+
+  const handleCreate = async () => {
+    if (username.trim() === "" || password.trim() === "") {
+      setError("Username and password cannot be empty.");
+      return;
+    }
+    try {
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      if (response.status === 409) {
+        throw new Error("Account already exists with that username or email");
+      }else if(!response.ok){
+        throw new Error("Create Account failed");
       }
 
       const data = await response.json();
@@ -40,6 +69,11 @@ function LogInPage({ onSubmit }) {
         type="text"
         value={username}
         onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
