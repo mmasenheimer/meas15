6a84f71e59fcb7615a6df4e8c0e789a94437f6b0
AlecSch1 <alecschmitt@arizona.edu>
2026-04-26 09:11:11 -0700

    map

--- src/FrontEnd/MainPage/Map.jsx
+++ src/FrontEnd/MainPage/Map.jsx
@@ -1,3 +1,4 @@
+import { set } from 'mongoose';
 import React, {useState} from 'react';
 
 export default function Map() {
@@ -5,6 +6,8 @@ export default function Map() {
     const [location1, setLocation1] = useState('');
     const [location2, setLocation2] = useState('');
     const [display, setDisplay] = useState(1);
+    const [routes, setRoutes] = useState(null);
+    const [selectedRoute, setSelectedRoute] = useState(null);
 
     const generateRoutes = async () => {
         if (location1.trim() === "" || location2.trim() === "") {
@@ -12,7 +15,7 @@ export default function Map() {
             return;
         }
         try {
-        const response = await fetch("/api/maps/getActivity", {
+        const response = await fetch("/api/map/getActivity", {
             method: "POST",
             headers: {
             "Content-Type": "application/json",
@@ -26,6 +29,7 @@ export default function Map() {
 
         const data = await response.json();
         console.log(data);
+        setRoutes(data);
         setDisplay(2);
         } catch (err) {
             setError(err.message || "An error occurred during create group");
@@ -54,12 +58,36 @@ export default function Map() {
             return (
                 <div id = "display2">
                     <h1>Generated Routes:</h1>
+                    {routes.map((route) => () => {
+                        <div classname = "route">
+                            <h1>{route.label}</h1>
+                            <text>  
+                                Arrives: {route.arrives}\n 
+                                Departs: {route.departs}\n
+                                Duration: {route.duration}\n
+                                Distance: {route.distance}\n
+                            </text>
+                            <h1>You can save {route.points} grams of carbon by choosing this route!</h1>
+                            <h1>Steps:</h1>
+                            {route.steps.map((step) => () => {
+                                <p className = "step">{step}</p>
+                            })}
+                        </div>
+                    })
+                    }
+                    <select value={selectedRoute} onChange={(e) => {setSelectedRoute(e.target.value); setDisplay(3)}}>
+                        <option value="">Select a route!</option>
+                        {routes.map((route) => (
+                            <option key={route.label} value={route.key()}>{route.label}</option>
+                        ))}
+                    </select>
                 </div>
             );
         } else if(display === 3) {
+            
             return (
                 <div id = "display3">
-                    <h1>You saved carbon!</h1>
+                    <h1>You saved {routes[selectedRoute].points} grams of carbon!</h1>
                 </div>
             );
         }
